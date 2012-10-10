32779e5bdb3f24effd7f91e0ba504beeb8383846
Cryptiv <quicktime115@gmail.com>
2012-10-09 23:22:55 -0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,91 +1,114 @@
+/*
+ * With this, any Plug.DJ user is able to turn their account into a fully-featured bot for their room.
+ * This script is protected under the Limited Use license (LU).
+ * Author: Harry J. (Royal_Soda)
+ * http://www.harry-j.com/
+*/
+
+
 //Welcome a user upon joining/entering.
-function join(user) {
+function join(user) { 
+
+    var welcomeArray = [
+        "Aloha",
+        "Howdy!",
+        "Welcome!",
+        "Hello, human!",
+        "Hola!",
+        "What's up?",
+        "Hey",
+        "Yo,",
+        "How have you been?,",
+        "sup,",
+        "Hi there"];
   
-//Print the message to chat.
-    API.sendChat("FARTS" + " @" + user.username)
-}
+    var randomN = Math.floor(Math.random() * welcomeArray.length); 
+  
+	//Print the message to chat.
+    API.sendChat(welcomeArray[randomN] + " @" + user.username) 
+} 
 
 //Say good-bye to the user.
 //The user leaving won't see this, as the listener only sees the user has left until they closed, reloaded or exited their window.
-function leave(user) {
+function leave(user) { 
 
-    var leaveArray = [
+    var leaveArray = [ 
         "@" + user.username + " left.",
         "Catch ya later, @" + user.username + ".",
         "Bye! @" + user.username];
 
-    var randomN = Math.floor(Math.random() * leaveArray.length);
+    var randomN = Math.floor(Math.random() * leaveArray.length); 
 
-//Print the message to chat.
+	//Print the message to chat.
     API.sendChat(leaveArray[randomN])
 
 }
 
 //Function called on upon a new chat message.
-function readChat(data) {
+function readChat(data) { 
 
-//Checks if a user tags your account in their message.
+	//Checks if a user tags your account in their message.
     if (data.message.indexOf("@" + API.getSelf().username) > -1) {
 
-            var isChat = false;
+            var isChat = false; 
 
-//Checks if the user stated 'what time is it'
+		//Checks if the user stated 'what time is it'
         if (data.message.indexOf('what time is it') != -1) {
-
-isChat = true;
-
-            var currentTime = new Date()
-            var hours = currentTime.getHours()
-            var minutes = currentTime.getMinutes()
-            var ampm
-            if (hours > 11) {
-                ampm = ("PM")
-            } else {
-                ampm = ("AM")
-            }
+		
+			isChat = true;
+			
+            var currentTime = new Date() 
+            var hours = currentTime.getHours() 
+            var minutes = currentTime.getMinutes() 
+            var ampm 
+            if (hours > 11) { 
+                ampm = ("PM") 
+            } else { 
+                ampm = ("AM") 
+            } 
   
-            hours = (hours > 12) ? hours - 12 : hours;
-            hours = (hours == '00') ? 12 : hours;
-            if (minutes < 10) {
-                minutes = "0" + minutes
-            }
-            var mytime = (hours + ":" + minutes + " " + ampm)
+            hours = (hours > 12) ? hours - 12 : hours; 
+            hours = (hours == '00') ? 12 : hours; 
+            if (minutes < 10) { 
+                minutes = "0" + minutes 
+            } 
+            var mytime = (hours + ":" + minutes + " " + ampm) 
 
-//Prints the time in chat.
-//The time is based off what time it is wherever your computer is located.
+			//Prints the time in chat.
+			//The time is based off what time it is wherever your computer is located.
             API.sendChat("It's " + mytime + " somewhere in the cyberspace!");
 
         }
   
         //Where are you from? Where do you live? How are you doing?
-//These are to make the bot seem a bit smarter.
+		//These are to make the bot seem a bit smarter.
         if (data.message.toLowerCase().indexOf('where') != -1) {
-
-isChat = true;
-
+		
+			isChat = true;
+		
             if (data.message.toLowerCase().indexOf('are') != -1) {
   
                 if (data.message.toLowerCase().indexOf('from') != -1) {
   
-                    API.sendChat("I'm from the Internet.. Here, I'll send you a map, so you can visit me : http://xkcd.com/802/");
-                }
-            }
+                    API.sendChat("I'm from the Internet.. Here, I'll send you a map, so you can visit me : http://xkcd.com/802/"); 
+                } 
+            } 
   
             if (data.message.toLowerCase().indexOf('do') != -1) {
-
+			
                 if (data.message.toLowerCase().indexOf('live') != -1) {
   
-                    API.sendChat("Right below the Blogosphere : http://xkcd.com/802/");
-                }
+                    API.sendChat("Right below the Blogosphere : http://xkcd.com/802/"); 
+                } 
   
-            }
+            } 
   
         }
-
+		
         if (data.message.toLowerCase().indexOf('how') != -1) {
-
-isChat = true;
-
+		
+			isChat = true;
+			
             if (data.message.toLowerCase().indexOf('are') != -1) {
   
                 if (data.message.toLowerCase().indexOf('doing') != -1) {
@@ -94,27 +117,27 @@ isChat = true;
                         "I'm just great, all systems are up and running!",
                         "I'm feeling a bit botty today",
                         "I'm doing fine, you? :) @" + data.from];
-
+				
                     var randomN = Math.floor(Math.random() * doingArray.length);
-
+					
                     API.sendChat(doingArray[doingN]);
   
                 }
             }
         }
 
-//If the message tagging your bot did not match any of the above.
+		//If the message tagging your bot did not match any of the above.
         if (isChat != true) {
-
+		
             var watArray = [
                 "I don't know what you're trying to tell me. @" + data.from + "",
                 "Hey, we bots need some sleep too.",
                 "I'm not in a chatty mood right now"];
   
-            var randomN = Math.floor(Math.random() * watArray.length);
+            var randomN = Math.floor(Math.random() * watArray.length); 
   
-            API.sendChat(watArray[randomN]);
-        }
+            API.sendChat(watArray[randomN]); 
+        } 
     }}
 
 //Calls the methods above when the listener is activated.
